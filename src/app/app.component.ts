004adf8a79086a907102a261d41185062d912e66
Ricardo <richiabv@gmail.com>
2024-09-27 08:31:33 -0500

    feat(sidenav-bar): added sidenav-bar to app.component.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SideNavigatorBarComponent} from "./public/components/side-navigator-bar/side-navigator-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideNavigatorBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
