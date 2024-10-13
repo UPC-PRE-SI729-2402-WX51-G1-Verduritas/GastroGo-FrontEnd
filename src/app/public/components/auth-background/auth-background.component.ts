ef3d2fc4eed6311f24c872c134b25d60dff0e653
Ricardo <richiabv@gmail.com>
2024-10-13 17:53:49 -0500

    feat(auth-background): created auth-background component

---
+++ src/app/public/components/auth-background/auth-background.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-background',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-background.component.html',
+  styleUrl: './auth-background.component.css'
+})
+export class AuthBackgroundComponent {
+
+}
