586697bc8ab5e36edd373edc10a65e33a0f0125e
Ricardo <richiabv@gmail.com>
2024-10-14 01:29:10 -0500

    chore: added routes for sign up and log in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,9 @@ import {HomeComponent} from "./public/pages/home/home.component";
 import {ProfileComponent} from "./workers/profile/page/my-profile/my-profile.component";
 import {OrderComponent}  from "./workers/my-order/pages/order/order.component";
 import {RestaurantComponent} from "./workers/restaurant/page/restaurant/restaurant.component";
+import {LogInComponent} from "./iam/pages/log-in/log-in.component";
+import {SignUpComponent} from "./iam/pages/sign-up/sign-up.component";
+import {AuthBackgroundComponent} from "./public/components/auth-background/auth-background.component";
 
 export const routes: Routes = [
   {path:'home',component: HomeComponent},
@@ -11,5 +14,8 @@ export const routes: Routes = [
   {path:'restaurant',component: RestaurantComponent},
   {path:'role',component: RolSectionComponent},
   {path:'order',component: OrderComponent},
+  {path: 'log-in', component: LogInComponent},
+  {path: 'sign-up', component: SignUpComponent},
+  {path: 'auth', component: AuthBackgroundComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
