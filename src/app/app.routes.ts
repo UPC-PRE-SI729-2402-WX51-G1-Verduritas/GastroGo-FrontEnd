4dfa25929ebb67943d1a3b23a61a818994dcf1b5
Ricardo <richiabv@gmail.com>
2024-10-18 11:39:21 -0500

    chore: added routes for iam components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,9 @@ import {RestaurantComponent} from "./workers/restaurant/page/restaurant/restaura
 import {LogInComponent} from "./iam/pages/log-in/log-in.component";
 import {SignUpComponent} from "./iam/pages/sign-up/sign-up.component";
 import {AuthBackgroundComponent} from "./public/components/auth-background/auth-background.component";
+import {PaymentComponent} from "./iam/components/payment.component/payment.component";
+import {PlanRestaurantComponent} from "./iam/components/plan-restaurant/plan-restaurant.component";
+import {PlanWorkersComponent} from "./iam/components/plan-workers/plan-workers.component";
 
 export const routes: Routes = [
   {path:'home',component: HomeComponent},
@@ -17,5 +20,8 @@ export const routes: Routes = [
   {path: 'log-in', component: LogInComponent},
   {path: 'sign-up', component: SignUpComponent},
   {path: 'auth', component: AuthBackgroundComponent},
+  {path: 'payment', component: PaymentComponent},
+  {path: 'plan-restaurant', component: PlanRestaurantComponent},
+  {path: 'plan-workers', component: PlanWorkersComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full' },
 ];
