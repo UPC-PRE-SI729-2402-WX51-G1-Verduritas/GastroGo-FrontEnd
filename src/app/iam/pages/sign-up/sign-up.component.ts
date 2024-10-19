d7023eed5aa6b21b2c441f32dd6cad1c29512586
Ricardo <richiabv@gmail.com>
2024-10-18 20:30:33 -0500

    chore: add routes for sign up component

--- src/app/iam/pages/sign-up/sign-up.component.ts
+++ src/app/iam/pages/sign-up/sign-up.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {AuthBackgroundComponent} from "../../../public/components/auth-background/auth-background.component";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-sign-up',
@@ -11,5 +12,12 @@ import {AuthBackgroundComponent} from "../../../public/components/auth-backgroun
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  constructor(private router: Router) {}
+  goToSignUpWorkers() {
+    this.router.navigate(['sign-up-workers']);
+  }
+  goToSignUpRestaurants() {
+    this.router.navigate(['sign-up-restaurant']);
+  }
 
 }
