d072d5f4b0b74711ebe90a1ab366f533f399b440
Rendery77 <rendery77@gmail.com>
2024-10-22 00:31:02 -0500

    feat(profile): update view profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {ProfileComponent} from "./profile/page/my-profile/my-profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'profile',component: ProfileComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full' },
+];
