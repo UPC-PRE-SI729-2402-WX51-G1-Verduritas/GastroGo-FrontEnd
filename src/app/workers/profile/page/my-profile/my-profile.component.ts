5180e071ceb954eed0cb72f1cecdc0d14173e7a1
Rendery77 <rendery77@gmail.com>
2024-10-23 23:19:01 -0500

    feat(profile): update my profile view

--- src/app/workers/profile/page/my-profile/my-profile.component.ts
+++ src/app/workers/profile/page/my-profile/my-profile.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import {SideNavigatorBarComponent} from "../../../../public/components/side-navigator-bar/side-navigator-bar.component";
 @Component({
   selector: 'app-profile',
   standalone: true,
-    imports: [CommonModule, MatInputModule, MatButtonModule, MatCheckboxModule, SideNavigatorBarComponent],
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatCheckboxModule],
   templateUrl: './my-profile.component.html',
   styleUrl: './my-profile.component.css'
 })
