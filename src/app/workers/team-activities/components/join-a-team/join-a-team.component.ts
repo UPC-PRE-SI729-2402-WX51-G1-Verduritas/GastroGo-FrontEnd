eff82410e1ef538a38d18d9b964afc89606c8ec6
FABIOLA <ximemegan1302@gmail.com>
2024-10-24 17:22:07 -0500

    feat(join-a-team): created join-a-team components

---
+++ src/app/workers/team-activities/components/join-a-team/join-a-team.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-a-team',
+  standalone: true,
+  imports: [],
+  templateUrl: './join-a-team.component.html',
+  styleUrl: './join-a-team.component.css'
+})
+export class JoinATeamComponent {
+
+}
