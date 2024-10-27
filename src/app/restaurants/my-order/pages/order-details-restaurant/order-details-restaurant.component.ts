be99e06da29b981f7a72cc6522ca2395113e6c16
JoaquinPedraza1 <144255407+JoaquinPedraza1@users.noreply.github.com>
2024-10-27 13:10:50 -0500

    feat(my-orders)Update order-details-restaurant.component.ts

--- src/app/restaurants/my-order/pages/order-details-restaurant/order-details-restaurant.component.ts
+++ src/app/restaurants/my-order/pages/order-details-restaurant/order-details-restaurant.component.ts
@@ -3,14 +3,15 @@ import {CommonModule} from "@angular/common";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-order-details-restaurant',
   standalone: true,
-  imports:  [CommonModule, MatInputModule, MatButtonModule, MatCheckboxModule],
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatCheckboxModule, RouterLink],
   templateUrl: './order-details-restaurant.component.html',
   styleUrl: './order-details-restaurant.component.css'
 })
 export class OrderDetailsRestaurantComponent {
-
+  cancelButton: boolean=false;
 }
