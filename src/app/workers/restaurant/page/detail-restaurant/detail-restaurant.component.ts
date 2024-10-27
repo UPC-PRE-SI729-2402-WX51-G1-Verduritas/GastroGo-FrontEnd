14a9a139660bc980c98443b86127bc7b138f4a65
Rendery77 <rendery77@gmail.com>
2024-10-27 17:48:54 -0500

    feat(detail-restaurant): create detail-restaurant view

---
+++ src/app/workers/restaurant/page/detail-restaurant/detail-restaurant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-restaurant',
+  standalone: true,
+  imports: [],
+  templateUrl: './detail-restaurant.component.html',
+  styleUrl: './detail-restaurant.component.css'
+})
+export class DetailRestaurantComponent {
+
+}
