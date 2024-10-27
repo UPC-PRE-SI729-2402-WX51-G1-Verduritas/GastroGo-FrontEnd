d1dd3cb7c1853d1ab1750bb771d1d74bdde5a0e9
Rendery77 <rendery77@gmail.com>
2024-10-27 18:35:13 -0500

    feat(menu): fix padding

--- src/app/restaurants/menu/pages/menu/menu/menu.component.ts
+++ src/app/restaurants/menu/pages/menu/menu/menu.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {
+  SideNavigatorBarComponent
+} from "../../../../../public/components/side-navigator-bar/side-navigator-bar.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [SideNavigatorBarComponent,RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
