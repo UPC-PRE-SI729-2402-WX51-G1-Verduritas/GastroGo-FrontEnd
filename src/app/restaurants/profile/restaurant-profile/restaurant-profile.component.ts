a842a01dd94a7cdad1a35f49571203ce63506931
FABIOLA <ximemegan1302@gmail.com>
2024-10-27 08:21:27 -0500

    feat(restaurant-profile): added restaurant-profile.component.ts

--- src/app/restaurants/profile/restaurant-profile/restaurant-profile.component.ts
+++ src/app/restaurants/profile/restaurant-profile/restaurant-profile.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {MatFormField} from "@angular/material/select";
+import {MatInput} from "@angular/material/input";
+import {MatButton} from "@angular/material/button";
+import {FormsModule} from "@angular/forms";
+import {MatCheckbox} from "@angular/material/checkbox";
+import {SideNavigatorBarComponent} from "../../../public/components/side-navigator-bar/side-navigator-bar.component";
 
 @Component({
   selector: 'app-restaurant-profile',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCheckbox,
+    FormsModule,
+    MatButton,
+    MatInput,
+    MatFormField,
+    SideNavigatorBarComponent
+  ],
   templateUrl: './restaurant-profile.component.html',
   styleUrl: './restaurant-profile.component.css'
 })
 export class RestaurantProfileComponent {
+  name: string = '';
+  lastNames: string = '';
+  restaurantName: string = '';
+  telephoneNumber: string = '';
+  address: string = '';
+  email: string = '';
+  password: string = '';
+  dni: string = '';
+  ruc: string = '';
+  monthlyPayment: boolean = false;
+  annualPayment: boolean = false;
+
 
 }
