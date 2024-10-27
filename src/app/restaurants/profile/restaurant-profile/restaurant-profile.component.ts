a0f3486bcf9c96eb891ed391eb6718ae525fd097
FABIOLA <ximemegan1302@gmail.com>
2024-10-27 08:00:09 -0500

    feat(restaurant-profile): created restaurant-profile components

---
+++ src/app/restaurants/profile/restaurant-profile/restaurant-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './restaurant-profile.component.html',
+  styleUrl: './restaurant-profile.component.css'
+})
+export class RestaurantProfileComponent {
+
+}
