4c276a0a968a560e9e857f4bc760180873e2f15b
JoaquinPedraza1 <u202218514@upc.edu.pe>
2024-10-27 12:02:56 -0500

    feat(my-orders) created Order tracker

---
+++ src/app/workers/my-order/pages/order-tracker/order-tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'app-order-tracker',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './order-tracker.component.html',
+  styleUrl: './order-tracker.component.css'
+})
+export class OrderTrackerComponent {
+
+}
