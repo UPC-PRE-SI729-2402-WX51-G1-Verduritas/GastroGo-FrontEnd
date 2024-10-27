4a38fb6899935c25c7f0482eccdf546feb25ec24
Rendery77 <rendery77@gmail.com>
2024-10-27 17:15:09 -0500

    feat(order-tracker): import SideNav

--- src/app/workers/my-order/pages/order-tracker/order-tracker.component.ts
+++ src/app/workers/my-order/pages/order-tracker/order-tracker.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
+import {SideNavigatorBarComponent} from "../../../../public/components/side-navigator-bar/side-navigator-bar.component";
 
 @Component({
   selector: 'app-order-tracker',
   standalone: true,
   imports: [
-    RouterLink,
-       SideNavigatorBarComponent
+    RouterLink,SideNavigatorBarComponent
   ],
   templateUrl: './order-tracker.component.html',
   styleUrl: './order-tracker.component.css'
