d1459393e5ebfc2691a4cc4e59d236125b3b841d
JoaquinPedraza1 <u202218514@upc.edu.pe>
2024-10-27 15:20:15 -0500

    feat(my-orders) Created Update status componet

---
+++ src/app/restaurants/my-order/pages/update-status/update-status.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-status',
+  standalone: true,
+  imports: [],
+  templateUrl: './update-status.component.html',
+  styleUrl: './update-status.component.css'
+})
+export class UpdateStatusComponent {
+
+}
