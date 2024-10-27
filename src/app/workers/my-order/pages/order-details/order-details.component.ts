6c2670023c425a9aa17f656cafd2cc1479fb22dc
JoaquinPedraza1 <144255407+JoaquinPedraza1@users.noreply.github.com>
2024-10-27 12:15:50 -0500

    feat (my-orders): Modify order-details.component.ts

--- src/app/workers/my-order/pages/order-details/order-details.component.ts
+++ src/app/workers/my-order/pages/order-details/order-details.component.ts
@@ -3,11 +3,13 @@ import {CommonModule} from "@angular/common";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCheckboxModule} from "@angular/material/checkbox";
+import {SideNavigatorBarComponent} from "../../../../public/components/side-navigator-bar/side-navigator-bar.component";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-order-details',
   standalone: true,
-  imports:  [CommonModule, MatInputModule, MatButtonModule, MatCheckboxModule],
+  imports: [CommonModule, MatInputModule, MatButtonModule, MatCheckboxModule, SideNavigatorBarComponent, RouterLink],
   templateUrl: './order-details.component.html',
   styleUrl: './order-details.component.css'
 })
