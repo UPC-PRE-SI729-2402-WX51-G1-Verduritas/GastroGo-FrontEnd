7b6870866071f3812090728c03288a44161a14ba
Rendery77 <rendery77@gmail.com>
2024-10-27 20:24:31 -0500

    feat(alerts): create alerts for profile

---
+++ src/app/workers/profile/page/alerts/payment-method-added/payment-method-added.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-method-added',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-method-added.component.html',
+  styleUrl: './payment-method-added.component.css'
+})
+export class PaymentMethodAddedComponent {
+
+}
---
+++ src/app/workers/profile/page/alerts/payment-method-not-added/payment-method-not-added.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-method-not-added',
+  standalone: true,
+  imports: [],
+  templateUrl: './payment-method-not-added.component.html',
+  styleUrl: './payment-method-not-added.component.css'
+})
+export class PaymentMethodNotAddedComponent {
+
+}
