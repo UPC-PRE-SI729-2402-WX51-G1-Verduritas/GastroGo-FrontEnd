ecdabbcbb4c5b33aa52cbcbefb7a1bfbf13d7fdc
Rendery77 <rendery77@gmail.com>
2024-10-27 20:46:28 -0500

    feat(payment-alerts): improvement alerts funtions

--- src/app/workers/profile/page/alerts/payment-method-added/payment-method-added.component.ts
+++ src/app/workers/profile/page/alerts/payment-method-added/payment-method-added.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-payment-method-added',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './payment-method-added.component.html',
   styleUrl: './payment-method-added.component.css'
 })
 export class PaymentMethodAddedComponent {
+  @Output() close = new EventEmitter<void>();
 
+  closeModal() {
+    this.close.emit();
+  }
 }
--- src/app/workers/profile/page/payment-method/payment-method.component.ts
+++ src/app/workers/profile/page/payment-method/payment-method.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import {SideNavigatorBarComponent} from "../../../../public/components/side-navigator-bar/side-navigator-bar.component";
+import {CommonModule} from "@angular/common";
+import {PaymentMethodAddedComponent} from "../alerts/payment-method-added/payment-method-added.component";
 
 @Component({
   selector: 'app-payment-method',
   standalone: true,
   imports: [
-    SideNavigatorBarComponent
+    SideNavigatorBarComponent, CommonModule, PaymentMethodAddedComponent
   ],
   templateUrl: './payment-method.component.html',
   styleUrl: './payment-method.component.css'
 })
 export class PaymentMethodComponent {
+  showModal = false;
 
+  confirmPayment() {
+    this.showModal = true; // Show
+  }
+
+  closeModal() {
+    this.showModal = false; // Close
+  }
 }
